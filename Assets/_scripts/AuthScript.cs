3f992aab94219caf256c8a1c89086d1e8c6d3d44
Akileswar <akil@innov8labs.in>
2018-02-19 17:21:18 +0530

    First successful build files

---
+++ Assets/_scripts/AuthScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+
+public class AuthScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
